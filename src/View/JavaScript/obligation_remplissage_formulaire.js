df7b7d0a8b99d99982190affe9d88027daf863a1
Jipmaa <f.thunevin@gmail.com>
2025-03-30 14:05:26 +0200

    Modifications mineurs creationuser avec ajout css et js

---
+++ src/View/JavaScript/obligation_remplissage_formulaire.js
@@ -0,0 +1,13 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".creation").addEventListener("submit", function (event) {
+        let role = document.getElementById("role").value;
+        let pole = document.querySelector("input[name='pole']").value.trim();
+
+        if (role === "null" || department === "null") {
+            alert("Veuillez remplir tous les champs.");
+            event.preventDefault(); // Empêche l'envoi du formulaire
+        }
+    });
+});
+</script>
