a8e1a504ccdc8ba4462058fdfa3cf25b7d81c0e9
Gaelle Le Boulicaut <lbg.gaelle@gmail.com>
2025-04-07 06:59:51 +0200

    changement icone bouton deconnexion

--- src/View/JavaScript/menulateral.js
+++ src/View/JavaScript/menulateral.js
@@ -1,6 +1,12 @@
 let boutonMenuLateral = document.getElementById("bouton-lateral");//boutton
 let formulaireMenuLateral = document.getElementById("formulaire-lateral");//div du formulaire
+let icone = document.getElementById("icone-lateral");
 
 boutonMenuLateral.addEventListener("click", () => {
     formulaireMenuLateral.classList.toggle('active');
+    if (formulaireMenuLateral.classList.contains("active")) {
+        icone.name = "close";
+    }else{
+        icone.name = "menu";
+    }
 });
